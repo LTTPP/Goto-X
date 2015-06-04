7c357c252c081e4aa4b842d3424c9de78c4b0eaf
liujianwei <liujianwei.me@gmail.com>
2015-06-05 00:34:52 +0800

    use JSON 2.1.4 to replace JSON 1.x

--- chrome/js/tlds.js
+++ chrome/js/tlds.js
@@ -1,33 +1,11 @@
 var domains = [];
 
 function parseDomains() {
-	console.log("start parsing domain.json");
-	$.ajax({
-		url : "../data/tlds.json",
-		success : function(data) {
-			domains = JSON.parse(data);
-			console.log("got data from domain.json: " + domains[10]);
-		}
-	});
-
-	console.log("---------------------------");
-
-	$.ajax({
-		url : "../data/tlds.json",
-		success : function(data) {
-			domains = $.parseJSON(data);
-			console.log("got data from domain.json: " + domains[10]);
-		}
-	});
-
-	console.log("+++++++++++++++++++++++++++");
-
+	console.log("start parsing tlds.json");
 	$.getJSON("../data/tlds.json", function(data) {
 		domains = data;
-		console.log("got data from domain.json: " + data[10]);
+		console.log("finish parsing tlds.json");
 	});
-
-	console.log("end parsing domain.json");
 }
 
 function getDomains() {
